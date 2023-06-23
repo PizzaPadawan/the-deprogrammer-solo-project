9909224ad3b4c020f0905ee72cc6bd787a22536d
Kord Maurer <kord.r.maurer@gmail.com>
2023-06-23 13:15:52 -0500

    added sort function to game reducer to return total tallies for gameplay in order descending by total column value

--- src/redux/reducers/game.reducer.js
+++ src/redux/reducers/game.reducer.js
@@ -1,6 +1,9 @@
 function gameReducer(state = [], action) {
     switch (action.type) {
         case "SET_GAMELIST":
+            action.payload.sort((a, b) => 
+            a.result.total > b.result.total ? -1 : 
+            a.result.total < b.result.total ? +1 : 0 )
             return action.payload;
         default:
             return state;
