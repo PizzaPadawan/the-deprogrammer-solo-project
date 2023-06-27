74d14934061c037b060580ea5b1cbe48024e912c
Kord Maurer <kord.r.maurer@gmail.com>
2023-06-26 21:58:17 -0500

    start styling Footer

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import { Typography, BottomNavigation, BottomNavigationAction } from '@mui/material';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+
 
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
@@ -6,7 +11,12 @@ import React from 'react';
 // or even care what the redux state is, so it doesn't need 'connect()'
 
 function Footer() {
-  return <footer>&copy; 2023 Kord Maurer</footer>;
+  return (<BottomNavigation>
+    <Typography variant="body2">&copy; 2023 Kord Maurer</Typography>
+    <BottomNavigationAction label="LinkedIn" icon={<LinkedInIcon />} />
+    <BottomNavigationAction label="GitHub" icon={<GitHubIcon />} />
+    <BottomNavigationAction label="Email" icon={<MailOutlineIcon />} />
+  </BottomNavigation>)
 }
 
 export default Footer;
