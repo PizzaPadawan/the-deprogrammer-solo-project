738a93d25a167795f8327c376ba5f5ec9104448f
Kord Maurer <kord.r.maurer@gmail.com>
2023-07-01 13:46:00 -0500

    add code comments to PlayPage

--- src/components/Pages/PlayPage/PlayPage.jsx
+++ src/components/Pages/PlayPage/PlayPage.jsx
@@ -1,11 +1,10 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import moment from "moment";
 //MUI
 import {
     Button,
     IconButton,
-    Tooltip,
     TableContainer,
     Table,
     TableHead,
@@ -25,14 +24,13 @@ import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 
 export default function PlayPage() {
 
+    // Redux
     // stick 'em up! gimme your WHOLE STORE
     const masterlist = useSelector(store => store.masterlist);
-    const panelUsers = useSelector(store => store.toplist.panelUserReducer);
     const toplist = useSelector(store => store.toplist.toplistReducer);
     const gamelist = useSelector(store => store.game);
     const user = useSelector(store => store.user);
     const currentList = useSelector(store => store.currentList)
-
     const dispatch = useDispatch();
 
     // instantiating useRef for setInterval
@@ -42,6 +40,8 @@ export default function PlayPage() {
     // fire setInterval if our masterlist is in game_mode = true,
     // and clearInterval if changed to false
     useEffect(() => {
+        // activates our setInterval to dispatch Game List and Masterlist every 2 seconds if our current masterlist is set to game_mode === true
+        // This makes it so our Game List will update in "real time" for every user
         intervalRef.current = setInterval(() => {
             if (masterlist.length > 0 && masterlist[0].game_mode) {
                 dispatch({
@@ -59,10 +59,11 @@ export default function PlayPage() {
             }
         }, 2000);
 
+        // clears our setInterval if the conditional above is not met
         return () => {
             clearInterval(intervalRef.current);
         };
-
+        // dependent on the change of masterlist store
     }, [masterlist])
 
     // page load dispatches
@@ -150,18 +151,26 @@ export default function PlayPage() {
         <Container maxWidth="xl">
             <Paper sx={{ p: 3, my: 5 }}>
                 <Grid container spacing={2}>
+
+                    {/* Display artist name as page header if our masterlist is populated and in game mode */}
                     {masterlist.length > 0 && masterlist[0].game_mode === true &&
                         <Grid item xs={11}>
-                            <Typography variant="h4" align="left">{masterlist[0].artist}</Typography>
+                            <Typography
+                                variant="h4"
+                                component="h2"
+                                align="left"
+                            >{masterlist[0].artist}
+                            </Typography>
                         </Grid>
                     }
+
                     {/* admin features */}
                     {user.is_admin &&
                         <Grid item xs={1}>
                             {masterlist.length > 0 && masterlist[0].game_mode
                                 ? <>
                                     {/* if our masterlist is populated and game_mode is true, display these.
-                        First button below is the button to Stop the game */}
+                                    First button below is the button to Stop the game */}
                                     <Button
                                         variant="contained"
                                         color="warning"
@@ -169,7 +178,7 @@ export default function PlayPage() {
                                     >STOP<StopIcon />
                                     </Button>
                                 </>
-                                // Button to 
+                                // Button to Start the game
                                 : <Button
                                     variant="contained"
                                     color="warning"
@@ -179,9 +188,11 @@ export default function PlayPage() {
                             }
                         </Grid>
                     }
+
                     {/* below is everything that will display for users once the admin has marked this masterlist's game_mode as TRUE */}
                     {masterlist.length > 0 && masterlist[0].game_mode === true
                         ? <>
+                            {/* Toplist table */}
                             <Grid item xs={4}>
                                 <TableContainer sx={{ maxHeight: 400 }}>
                                     <Table stickyHeader>
@@ -200,6 +211,7 @@ export default function PlayPage() {
                                                     return (
                                                         !track.hidden
                                                             ? track.is_played
+                                                                // tracks that have been played will be greyed out
                                                                 ? <TableRow
                                                                     sx={{ backgroundColor: 'darkgray' }}
                                                                     key={track.id}>
@@ -207,17 +219,20 @@ export default function PlayPage() {
                                                                     <TableCell sx={{ fontSize: 11 }}>{track.album}</TableCell>
                                                                     <TableCell sx={{ fontSize: 11 }} colSpan={2} >{track.notes}</TableCell>
                                                                 </TableRow >
+                                                                // regular display of toplist row
                                                                 : <StyledTableRow key={track.id}>
                                                                     <StyledTableCell>{track.track}</StyledTableCell>
                                                                     <StyledTableCell>{track.album}</StyledTableCell>
                                                                     <StyledTableCell>{track.notes}</StyledTableCell>
                                                                     <StyledTableCell>
+                                                                        {/* Button to mark a song as is_played === true */}
                                                                         <IconButton
                                                                             onClick={() => playSong(track.masterlist_id)}
                                                                         ><PlayArrowIcon />
                                                                         </IconButton>
                                                                     </StyledTableCell>
                                                                 </StyledTableRow >
+                                                            // Don't display users' hidden toplist tracks 
                                                             : null
                                                     )
                                                 })
@@ -227,8 +242,10 @@ export default function PlayPage() {
                                 </TableContainer>
                             </Grid>
 
+                            {/* Game list table */}
                             <Grid item xs={8}>
-                                {/* once we've added to our gamelist, start rendering our Table */}
+                                {/* once we've started marking tracks as is_played === true, 
+                                they will be added to our gamelist and our Table will render */}
                                 {gamelist.length > 0 &&
                                     <TableContainer sx={{ maxHeight: 400 }}>
                                         <Table stickyHeader >
@@ -236,17 +253,20 @@ export default function PlayPage() {
                                                 <StyledTableRow >
                                                     <StyledTableCell>Track</StyledTableCell>
                                                     <StyledTableCell>Album</StyledTableCell>
+                                                    {/* return the result object keys as an array, then map over them to return result column titles */}
                                                     {Object.keys(gamelist[0].result).map((result, index) => {
-                                                            return (<StyledTableCell key={index}>{result}</StyledTableCell>)
-                                                        })}
+                                                        return (<StyledTableCell key={index}>{result}</StyledTableCell>)
+                                                    })}
                                                 </StyledTableRow >
                                             </TableHead>
                                             <TableBody>
+                                                {/* map over each track in our gamelist so it populates as songs are played */}
                                                 {gamelist.map(track => {
                                                     return (
                                                         <StyledTableRow key={track.track} >
                                                             <StyledTableCell>{track.track}</StyledTableCell>
                                                             <StyledTableCell>{track.album}</StyledTableCell>
+                                                            {/* return the result object values as an array, then map over them to return result numbers */}
                                                             {Object.values(track.result).map((result, index) => {
                                                                 return (<StyledTableCell key={index}>{result}</StyledTableCell>)
                                                             })}
@@ -260,6 +280,7 @@ export default function PlayPage() {
                                 }
                             </Grid>
                         </>
+
                         : <>
                             {/* if you visit this page outside of gameplay, this will be the message */}
                             <Grid item>
